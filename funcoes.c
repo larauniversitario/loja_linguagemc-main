d299de35117ffc6605e62bd1f5cf7315200d51b6
carolvfq <carolvfdq@gmail.com>
2025-05-05 16:21:42 -0300

    estruturando o projeto

---
+++ funcoes.c
@@ -0,0 +1,44 @@
+//funçãoes implementadas
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "funcoes.h"
+
+static int contador_produto = 0;
+static int contador_carrinho = 0;
+static itemCarrinho carrinho[50];
+
+//vou implementar todas as funçoes ainda 
+void infoProduto(Produto prod){
+
+}
+  
+void salvarProduto(Produto p){
+
+}
+void carregarProdutos(){
+
+}
+void listarProdutos(){
+
+}
+void cadstrarProdutodos(){
+
+}
+void visualizarCarrinho(){
+
+}
+void fecharPedido(){
+
+}
+Produto pegarProdutoPorCodigo(int codigo){
+
+}
+int * temNoCarrinho(int codigo){
+
+}
+
+void menu(){
+
+}
